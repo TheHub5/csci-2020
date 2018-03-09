9982d59019a40dea3058807e911dbe71d6a29963
Andrei Stoica <andrei.stoica.365@gmail.com>
2018-03-09 16:00:44 -0500

    Lab 07 demo

---
+++ lab07/src/main/java/Lab07Demo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.List;
+import java.util.stream.*;
+import java.util.stream.Collectors.*;
+
+class Lab07Demo {
+	public static void main (String[] args) throws IOException {
+		String inFile = "weatherwarnings-2015.csv";
+		String delimiter = ",";
+
+		BufferedReader br = new BufferedReader(new FileReader(inFile));
+
+		List<String> lineList = br.lines().map(Object::toString)
+																			.collect(Collectors.toList());
+
+		br.close();
+
+		for (String line :  lineList) {
+			String[] data = line.split(delimiter);
+			System.out.println(String.join(" | ", data));
+		}
+	}
+}
